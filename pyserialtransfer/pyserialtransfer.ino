13a7038f16f3140638e6d521ea0f2739a1cda6cc
Mehmet Mucahit KAYA <39586436+mmucahitkaya@users.noreply.github.com>
2022-09-11 13:13:46 +0300

    added potentiometer codes

--- pyserialtransfer/pyserialtransfer.ino
+++ pyserialtransfer/pyserialtransfer.ino
@@ -1,5 +1,7 @@
 #include "SerialTransfer.h"
-
+#define pot_1 A2 
+#define pot_2 A3 
+#define RST_PIN 9
 
 SerialTransfer myTransfer;
 
@@ -26,7 +28,7 @@ void setup()
   SerialUSB.begin(115200);
   Serial1.begin(115200);
   myTransfer.begin(Serial1);
-
+  analogReadResolution(12);
   sensorStruct.sensor_1 = 3.5;
   sensorStruct.sensor_2 = 4.5;
   pantiltStruct.pan = 60 ;
@@ -38,6 +40,10 @@ void loop()
 {
   // use this variable to keep track of how many
   // bytes we're stuffing in the transmit buffer
+  float sensor_1 = analogRead(pot_1);
+  float sensor_2 = analogRead(pot_2);
+  sensorStruct.sensor_1 = sensor_1;
+  sensorStruct.sensor_2 = sensor_2;
   uint16_t sendSize = 0;
 
   ///////////////////////////////////////// Stuff buffer with struct
